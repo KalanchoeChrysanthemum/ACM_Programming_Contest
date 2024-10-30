221c8e7815cc1837f33d7197b69ee0eeae1b5e9a
KalanchoeChrysanthemum <157724777+KalanchoeChrysanthemum@users.noreply.github.com>
2024-10-30 10:18:37 -0400

    Adding ACM problem PDFs

---
+++ 2022/Python/Problem_E/skylight.py
@@ -0,0 +1,42 @@
+import math
+import scipy
+
+r = 0
+
+def main():
+
+    answers = []
+
+    num_cases = int(input())
+
+    for _ in range(0, num_cases):
+        s, r = input().split(' ')
+
+        s = int(s)
+        r = int(r)
+
+
+        subintervals = 16
+
+        for _ in range(0, subintervals):
+
+        Area = step/3 (f(0)) + 4 * (sum of odd) + 2 * (sum of even) + f(1)
+
+
+def amount_covered(s, r) -> float:
+
+    half_s = s / 2
+
+    if r <= half_s:
+        return (3.14159265358979) * (r ** 2)
+    elif r >= math.sqrt((half_s ** 2) + (half_s ** 2)):
+        return s ** 2
+    else:
+        return
+
+
+def func(x):
+    return math.sqrt((r ** 2) - (x ** 2))
+
+if __name__ == '__main__':
+    main()
